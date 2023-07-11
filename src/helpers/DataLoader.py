9e3c6f576ae7dc61d51a5562c88e0e92b0d397af
mmartinezh21 <if.mmartinezh@gmail.com>
2023-07-11 00:51:17 -0600

    Objeto DataLoader creado

---
+++ src/helpers/DataLoader.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+class Dataloader:
+    def __init__(self, path):
+        self.path = path
+    def load_data(self):
+        data = pd.read_csv(self.path)
+        return data
